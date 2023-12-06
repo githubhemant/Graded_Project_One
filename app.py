bc4831f0af6f788a3d978b76786258342ba505ee
Hemant Pratap <hojha56@gmail.com>
2023-12-07 02:03:43 +0530

    Added Model for Computation

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-app = Flask(__name__)
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! Docker Test Hemant</p>"
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
